1d9ae071ed09ffdf93be1aa9c17265e9a12f50bd
CeballosLeandro <86359478+CeballosLeandro@users.noreply.github.com>
2023-11-23 19:55:12 -0300

    Commit db
    db change for import phpmyadmin

--- databases/museo-vf.sql
+++ databases/museo-vf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2023 a las 00:50:45
+-- Tiempo de generación: 23-11-2023 a las 23:52:35
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -19,9 +19,9 @@ SET time_zone = "+00:00";
 
 --
 -- Base de datos: `museo`
---	
-	CREATE DATABASE museo;
-	USE museo;
+--
+CREATE DATABASE museo;
+USE museo;
 -- --------------------------------------------------------
 
 --
@@ -37,6 +37,16 @@ CREATE TABLE `historial` (
   `motivobaja` varchar(100) DEFAULT NULL,
   `lugar` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `historial`
+--
+
+INSERT INTO `historial` (`id`, `idobjeto`, `fechaAlta`, `fechaBaja`, `posicion`, `motivobaja`, `lugar`) VALUES
+(1, 1, '2023-11-23 15:29:11', '2023-11-23 18:32:15', '12SCP', 'Reparación', 'Depósito'),
+(2, 1, '2023-11-23 15:34:47', '2023-11-23 18:36:02', '12SCP', 'Reparación', 'Depósito'),
+(3, 1, '2023-11-23 15:36:49', NULL, '11SCP', NULL, NULL),
+(4, 2, '2023-11-23 15:39:04', NULL, '10SME', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -56,6 +66,28 @@ CREATE TABLE `objeto` (
   `estado` enum('Activo','Inactivo') NOT NULL DEFAULT 'Activo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `objeto`
+--
+
+INSERT INTO `objeto` (`id`, `nombre`, `especialidad`, `posicion`, `imagen`, `descripcion`, `descripcionH`, `fechaIngreso`, `estado`) VALUES
+(1, 'AMD Am286', 'Computación', '11SCP', 'assets/imgobject/AMD286.jpg', 'El AMD Am286 es procesador que es una copia del Intel 80286, creado con permiso de Intel.', 'Esto fue así porque IBM quería que Intel, que era el proveedor de procesadores para el IBM PC, tuviese una segunda fuente para poder suplir la demanda en caso de problemas, por lo que obligó a Intel a licenciar su tecnología a otro fabricante, en este caso AMD. Por lo tanto el Am286 es idéntico al Intel 80286. Posteriormente AMD lo vendió como procesador embebido. Como curiosidad, los Am286 está marcados con la frase (M) (C) INTEL 1982, como se puede ver en la foto.', '2023-11-23 18:29:11', 'Activo'),
+(2, 'Torno paralelo', 'Mecánica', '10SME', 'assets/imgobject/683px-HwacheonCentreLathe-headstock-mask_legend.jpg', 'El torno paralelo o mecánico es el tipo de torno que evolucionó partiendo de los tornos antiguos cuando se le fueron incorporando nuevos equipamientos que lograron convertirlo en una de las máquinas herramientas más importante que han existido. ', 'Para la fabricación en serie y de precisión han sido sustituidos por tornos copiadores, revólver, automáticos y de CNC. Para manejar bien estos tornos se requiere la pericia de profesionales muy bien cualificados, ya que el manejo manual de sus carros puede ocasionar errores, a menudo, en la geometría de las piezas torneadas.', '2023-11-23 18:39:04', 'Activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `expiration` datetime NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -74,7 +106,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `mail`, `password`) VALUES
-(1, 'carlos', 'carlos@g.com', '1234upc');
+(1, 'carlos', 'carlos@g.com', '$2y$10$EQ/GG0ZNLjuBQw3rZFb5kOAUlIzCPO6U7BK2mcAaB3LLgEbSAUCuy'),
+(3, 'Carlitos', 'carlitos@gmail.com', '$2y$10$/OQc2szNARkODgUP132iPuWO/suoT48E6kRCwx1mDArjCOF5e668G'),
+(4, 'lea', 'leancm@gmail.com', '$2y$10$7bwNMXPU9QrthzTRWU8XVuphTed1auqpRtNtdCRfc..afIdmc3L3y');
 
 --
 -- Índices para tablas volcadas
@@ -93,6 +127,12 @@ ALTER TABLE `objeto`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `tokens`
+--
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
@@ -106,19 +146,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `historial`
 --
 ALTER TABLE `historial`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `objeto`
 --
 ALTER TABLE `objeto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tokens`
+--
+ALTER TABLE `tokens`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
